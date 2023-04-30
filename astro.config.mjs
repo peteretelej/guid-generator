66960b817826ba82e192430b017a70adc193d0a0
Peter Etelej <peter@etelej.com>
2023-04-30 23:51:41 +0300

    Enable github site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  integrations: [react(), tailwind(), mdx()],
+  site: 'https://peteretelej.github.io',
+  base: '/guid-generator',
 });
