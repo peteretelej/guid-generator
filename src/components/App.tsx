6f999fa36489e2182af39f36f88f4a6e58449ac5
Peter Etelej <peter@etelej.com>
2023-04-30 22:02:55 +0300

    Add dark mode

---
+++ src/components/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("bg-gray-900", "text-white");
+    } else {
+      document.body.classList.remove("bg-gray-900", "text-white");
+      document.body.classList.add("bg-gray-100", "text-gray-900");
+    }
+  }, [darkMode]);
+
+  return (
+    <div className="min-h-screen">
+      <main className="p-4">
+        {/* Add your generator form, GUID list, and other components here */}
+      </main>
+    </div>
+  );
+};
+
+export default App;
